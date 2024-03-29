d2c010f4ad83faefafc0a24c2b987c246f258a20
Henrique Paixao <henriquemfpaixao@gmail.com>
2024-03-28 22:23:58 -0300

    component input

--- projeto-react-api-1/src/pages/Livros/Livros.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Livros() {
-    return(
-        <div>
-            <h1>Livro</h1>
-        </div>
-    )
-}
--- projeto-react-api-1/src/pages/NovoLivro/NovoLivro.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function NovoLivro() {
-    return(
-        <div>
-            <h1>NovoLivro</h1>
-        </div>
-    )
-}
